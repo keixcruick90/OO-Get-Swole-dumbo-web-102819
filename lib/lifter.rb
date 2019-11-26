301a14bb5cf989dc5dd585f8c6355370d1796f16
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-11-26 14:24:47 +0000

    Automatically backed up by Learn

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,36 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+
+  end
+
+  def gyms
+
+  end
+
+  def self.average_lift_total
+
+  end
+
+  def gym_membership_cost
+
+  end
+
+  def new_membership(cost, gym)
+    Membership.new(cost, self, gym)
   end
 end
